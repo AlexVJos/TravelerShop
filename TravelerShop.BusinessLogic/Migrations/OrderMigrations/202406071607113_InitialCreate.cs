4887a3ee5c886a518053b546acf0c28fdbd6e0b8
Alexandr <alexandrvjosan@gmail.com>
2024-06-07 21:24:02 +0300

    Migrations for order and order items db

---
+++ TravelerShop.BusinessLogic/Migrations/OrderMigrations/202406071607113_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿namespace TravelerShop.BusinessLogic.Migrations.OrderMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        OrderId = c.Int(nullable: false),
+                        Image = c.Binary(),
+                        Name = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Quantity = c.Int(nullable: false),
+                        SubTotal = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Orders", t => t.OrderId, cascadeDelete: true)
+                .Index(t => t.OrderId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        CartId = c.Int(nullable: false),
+                        BillingName = c.String(nullable: false),
+                        BillingEmail = c.String(nullable: false),
+                        BillingAddress = c.String(nullable: false),
+                        BillingPhone = c.String(nullable: false),
+                        Comments = c.String(maxLength: 150),
+                        ShippingAddress = c.String(nullable: false),
+                        OrderDate = c.DateTime(nullable: false),
+                        Status = c.Int(nullable: false),
+                        Total = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderItems", "OrderId", "dbo.Orders");
+            DropIndex("dbo.OrderItems", new[] { "OrderId" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderItems");
+        }
+    }
+}
--- TravelerShop.BusinessLogic/Migrations/OrderMigrations/Configuration.cs
+++ TravelerShop.BusinessLogic/Migrations/OrderMigrations/Configuration.cs
@@ -1,22 +1,21 @@
-﻿namespace TravelerShop.BusinessLogic.Migrations
+﻿namespace TravelerShop.BusinessLogic.Migrations.OrderMigrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<TravelerShop.BusinessLogic.DBModel.CartContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<TravelerShop.BusinessLogic.DBModel.OrderContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "TravelerShop.BusinessLogic.DBModel.CartContext";
+            MigrationsDirectory = @"Migrations\OrderMigrations";
         }
 
-        protected override void Seed(TravelerShop.BusinessLogic.DBModel.CartContext context)
+        protected override void Seed(TravelerShop.BusinessLogic.DBModel.OrderContext context)
         {
             //  This method will be called after migrating to the latest version.
-
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
         }
